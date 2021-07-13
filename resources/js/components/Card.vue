89872982adc5b03d2bb1fdaadd38b97028749ab7
Marce Marin <marcelo@webloftdesigns.com>
2021-07-13 13:03:34 -0300

    FC-740

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="float-right pb-4">
     <button
-      v-if="showButtonVimeo"
+      v-if="showButtonIntercom"
       @click="redireccionar()"
-      class="bg-transparent hover:bg-teal-500 text-teal-700 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded"
+      class="
+        bg-transparent
+        hover:bg-teal-500
+        text-teal-700
+        font-semibold
+        hover:text-white
+        py-2
+        px-4
+        border border-teal-500
+        hover:border-transparent
+        rounded
+      "
     >
       <font-awesome-icon :icon="faBus" />
       {{ __("Start tour") }}
@@ -12,7 +23,18 @@
     <button
       v-if="showButtonVimeo"
       @click="openModal"
-      class="bg-transparent hover:bg-teal-500 text-teal-700 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded"
+      class="
+        bg-transparent
+        hover:bg-teal-500
+        text-teal-700
+        font-semibold
+        hover:text-white
+        py-2
+        px-4
+        border border-teal-500
+        hover:border-transparent
+        rounded
+      "
     >
       <font-awesome-icon :icon="faFilm" />
       {{ __("See tutorial") }}
@@ -59,18 +81,15 @@ export default {
   },
   methods: {
     async getVimeoUrl() {
-      const {
-        data: tutorial,
-      } = await Nova.request().post(
+      const { data: tutorial } = await Nova.request().post(
         "/nova-vendor/popup-video/module_tutorial",
         { module: this.resourceName }
       );
-
       if (tutorial != "-1") {
         this.videoUrl = tutorial.url_video;
         this.tourUrl = tutorial.url_intercom;
-        this.showButtonVimeo = this.videoUrl != null ? true : false;
-        this.showButtonIntercom = this.tourUrl != null ? true : false;
+        this.showButtonVimeo = this.videoUrl != null;
+        this.showButtonIntercom = this.tourUrl != null;
       }
     },
 
